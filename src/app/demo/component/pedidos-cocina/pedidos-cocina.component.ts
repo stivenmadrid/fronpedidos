10864a6c019b2dad383b9ad39d1bd49bbe512bf3
GUILLERMO LANDAZURI AMAYA <GUILLERMO.LANDAZURI@HOTMAIL.COM>
2024-05-24 21:06:48 -0500

    componente cocina

---
+++ src/app/demo/component/pedidos-cocina/pedidos-cocina.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos-cocina',
+  standalone: true,
+  imports: [],
+  templateUrl: './pedidos-cocina.component.html',
+  styleUrl: './pedidos-cocina.component.scss'
+})
+export class PedidosCocinaComponent {
+
+}
